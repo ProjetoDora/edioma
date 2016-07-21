dcdbf13d8218881ce2233e3526696c8ded54ea76
anikarni <anikarni@gmail.com>
2016-07-20 22:22:26 -0300

    #6 - Adiciona testes unitarios para PureScript

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,16 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+import Test.Unit.Console (TESTOUTPUT)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+
+main :: forall e. Eff (console :: CONSOLE, testOutput :: TESTOUTPUT | e) Unit
+main = runTest do
+  suite "sync code" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
